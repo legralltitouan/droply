fc0032715119609a5324b71648aabb929d251eaa
Titouan LEGRALL <99889131+legralltitouan@users.noreply.github.com>
2026-05-25 09:20:57 +0200

    Add files via upload

---
+++ SettingsWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace QuickSend
+{
+    public partial class SettingsWindow : Window
+    {
+        private readonly MainWindow _mainWindow;
+
+        public SettingsWindow(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+
+            this.MouseDown += (s, e) => { if (e.LeftButton == MouseButtonState.Pressed) this.DragMove(); };
+
+            LoadCurrentSettings();
+        }
+
+        private void LoadCurrentSettings()
+        {
+            StartupCheck.IsChecked = _mainWindow.CurrentSettings.RunAtStartup;
+            ThemeCheck.IsChecked = _mainWindow.CurrentSettings.IsLightTheme;
+            WebhookBox.Text = _mainWindow.CurrentSettings.DiscordWebhook;
+        }
+
+        private void ThemeCheck_Click(object sender, RoutedEventArgs e)
+        {
+            _mainWindow.SetTheme(ThemeCheck.IsChecked ?? false);
+        }
+
+        private void SaveAndClose_Click(object sender, RoutedEventArgs e)
+        {
+            _mainWindow.CurrentSettings.RunAtStartup = StartupCheck.IsChecked ?? true;
+            _mainWindow.CurrentSettings.IsLightTheme = ThemeCheck.IsChecked ?? false;
+            _mainWindow.CurrentSettings.DiscordWebhook = WebhookBox.Text.Trim();
+
+            _mainWindow.SaveSettings();
+
+            this.Close();
+        }
+    }
+}
